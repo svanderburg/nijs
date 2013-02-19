e21f8ca837ff9dba1a0a67e692fb7a226bce1c91
Sander van der Burg <svanderburg@gmail.com>
2013-02-19 15:41:34 +0100

    Fix conversion process a bit, a function is forbidden but the rest is not

--- lib/nixToJS.nix
+++ lib/nixToJS.nix
@@ -11,6 +11,7 @@ let
     else if builtins.isString expr then "\"${expr}\""
     else if builtins.isList expr then "[ ${stdenv.lib.concatMapStrings (elem: nixToJS elem + ",\n") expr} ]"
     else if builtins.isAttrs expr then "{ ${stdenv.lib.concatMapStrings (elem: elem + " : ${nixToJS (builtins.getAttr elem expr)},\n") (builtins.attrNames expr)} }"
-    else throw "Cannot convert the expression to JavaScript!";
+    else if builtins.isFunction expr then throw "Cannot convert a Nix function to a JavaScript function!"
+    else "\"${expr}\""; # We assume it's a file
 in
 nixToJS expr
