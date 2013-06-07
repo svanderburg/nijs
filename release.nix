cb81a4b0fe782f8d1765748f8ff640a564aacbf2
Sander van der Burg <svanderburg@gmail.com>
2013-06-07 18:45:34 +0200

    Make nijs packages testable

--- release.nix
+++ release.nix
@@ -6,6 +6,35 @@ let
   pkgs = import nixpkgs { inherit system; };
   
   version = builtins.readFile ./version;
+  
+  importNijsPackage = {pkgsJsFile, attrName}:
+    let nijsInlineProxy = import ./lib/inlineProxy.nix {
+      inherit (pkgs) stdenv writeTextFile nodejs;
+    };
+    in
+    import (pkgs.stdenv.mkDerivation {
+      name = "importnijs-${attrName}.nix";
+        
+      buildCommand = nijsInlineProxy {
+        name = "${attrName}-buildCommand";
+        requires = [
+          { var = "fs"; module = "fs"; }
+          { var = "pkgsJsFile"; module = pkgsJsFile; }
+          { var = "nijs"; module = "${./.}/lib/nijs.js"; }
+        ];
+        code = ''
+          var expr = pkgsJsFile.pkgs['${attrName}']();
+          expr = new nijs.NixExpression('let\n' +
+              '  pkgs = import ${nixpkgs} { system = "${system}"; };\n' +
+              '  nijsFunProxy = import ${./.}/lib/funProxy.nix { inherit (pkgs) stdenv nodejs; };\n' +
+              '  nijsInlineProxy = import ${./.}/lib/inlineProxy.nix { inherit (pkgs) stdenv writeTextFile nodejs; };\n' +
+              'in\n' +
+              expr.value);
+            
+          fs.writeFileSync(process.env['out'], nijs.jsToNix(expr));
+        '';
+      };
+    });
 in
 rec {
   tarball = pkgs.stdenv.mkDerivation {
@@ -48,5 +77,23 @@ rec {
     proxytests = import ./tests/proxytests.nix {
       inherit pkgs;
     };
-  };
+    
+    pkgs = 
+      let
+        pkgsJsFile = "${./.}/tests/pkgs.js";
+      in
+      {
+        hello = importNijsPackage { inherit pkgsJsFile; attrName = "hello"; };
+        zlib = importNijsPackage { inherit pkgsJsFile; attrName = "zlib"; };
+        file = importNijsPackage { inherit pkgsJsFile; attrName = "file"; };
+        perl = importNijsPackage { inherit pkgsJsFile; attrName = "perl"; };
+        openssl = importNijsPackage { inherit pkgsJsFile; attrName = "openssl"; };
+        curl = importNijsPackage { inherit pkgsJsFile; attrName = "curl"; };
+        wget = importNijsPackage { inherit pkgsJsFile; attrName = "wget"; };
+        sumTest = importNijsPackage { inherit pkgsJsFile; attrName = "sumTest"; };
+        stringWriteTest = importNijsPackage { inherit pkgsJsFile; attrName = "stringWriteTest"; };
+        appendFilesTest = importNijsPackage { inherit pkgsJsFile; attrName = "appendFilesTest"; };
+        createFileWithMessageTest = importNijsPackage { inherit pkgsJsFile; attrName = "createFileWithMessageTest"; };
+      };
+    };
 }
