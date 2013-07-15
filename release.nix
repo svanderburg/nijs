4b4c3b3ef981d351d14b75a4d3c392a4e8871ec5
Sander van der Burg <svanderburg@gmail.com>
2013-07-15 14:20:05 +0200

    Changed tarball job to properly generate a Hydra build product

--- release.nix
+++ release.nix
@@ -10,65 +10,81 @@ let
   nijsImportPackage = import ./lib/importPackage.nix {
     inherit nixpkgs system;
   };
-in
-rec {
-  tarball = pkgs.stdenv.mkDerivation {
-    name = "nijs-${version}.tgz";
-    src = ./.;
-    buildPhase = "true";
-    installPhase = ''
-      mkdir -p package
-      cd package
-      cp -av $src/* .
-      cd ..
-      tar cfvz $out package
-    '';
+  
+  determineTarballPath = tarball: {
+    name = "nijs-tarball";
+    outPath = "${tarball}/tarballs/nijs-${version}.tgz";
   };
 
-  build = pkgs.nodePackages.buildNodePackage {
-    name = "nijs-${version}";
-    src = tarball;
+  jobs = {
+    tarball = {officialRelease ? false}:
   
-    deps = [
-      pkgs.nodePackages.nijs
-      pkgs.nodePackages.optparse
-    ];
-  };
-  
-  doc = pkgs.stdenv.mkDerivation {
-    name = "nijs-docs-${version}";
-    src = tarball;
-    
-    buildInputs = [ pkgs.rubyLibs.jsduck ];
-    buildPhase = "make duck";
-    installPhase = ''
-      mkdir -p $out/nix-support
-      cp -R build/* $out
-      echo "doc api $out" >> $out/nix-support/hydra-build-products
-    '';
-  };
-  
-  tests = {
-    proxytests = import ./tests/proxytests.nix {
-      inherit pkgs;
-    };
-    
-    pkgs = 
-      let
-        pkgsJsFile = "${./.}/tests/pkgs.js";
-      in
-      {
-        hello = nijsImportPackage { inherit pkgsJsFile; attrName = "hello"; };
-        zlib = nijsImportPackage { inherit pkgsJsFile; attrName = "zlib"; };
-        file = nijsImportPackage { inherit pkgsJsFile; attrName = "file"; };
-        perl = nijsImportPackage { inherit pkgsJsFile; attrName = "perl"; };
-        openssl = nijsImportPackage { inherit pkgsJsFile; attrName = "openssl"; };
-        curl = nijsImportPackage { inherit pkgsJsFile; attrName = "curl"; };
-        wget = nijsImportPackage { inherit pkgsJsFile; attrName = "wget"; };
-        sumTest = nijsImportPackage { inherit pkgsJsFile; attrName = "sumTest"; };
-        stringWriteTest = nijsImportPackage { inherit pkgsJsFile; attrName = "stringWriteTest"; };
-        appendFilesTest = nijsImportPackage { inherit pkgsJsFile; attrName = "appendFilesTest"; };
-        createFileWithMessageTest = nijsImportPackage { inherit pkgsJsFile; attrName = "createFileWithMessageTest"; };
+      pkgs.releaseTools.sourceTarball {
+        name = "nijs-tarball";
+        inherit version;
+        src = ./.;
+        inherit officialRelease;
+        distPhase = ''
+          mkdir -p package
+          cd package
+          cp -av $src/* .
+          cd ..
+          tar cfvz nijs-${version}.tgz package
+          mkdir -pv $out/tarballs
+          cp *.tgz $out/tarballs
+        '';
       };
-    };
-}
+  
+    build = {tarball ? jobs.tarball {} }:
+
+      pkgs.nodePackages.buildNodePackage {
+        name = "nijs-${version}";
+        src = determineTarballPath tarball;
+  
+        deps = [
+          pkgs.nodePackages.nijs
+          pkgs.nodePackages.optparse
+        ];
+      };
+  
+    doc = {tarball ? jobs.tarball {} }:
+    
+      pkgs.stdenv.mkDerivation {
+        name = "nijs-docs-${version}";
+        src = determineTarballPath tarball;
+    
+        buildInputs = [ pkgs.rubyLibs.jsduck ];
+        buildPhase = "make duck";
+        installPhase = ''
+          mkdir -p $out/nix-support
+          cp -R build/* $out
+          echo "doc api $out" >> $out/nix-support/hydra-build-products
+        '';
+      };
+  
+    tests = {
+      proxytests = import ./tests/proxytests.nix {
+        inherit pkgs;
+      };
+    
+      pkgs = 
+        let
+          pkgsJsFile = "${./.}/tests/pkgs.js";
+        in
+        {
+          hello = nijsImportPackage { inherit pkgsJsFile; attrName = "hello"; };
+          zlib = nijsImportPackage { inherit pkgsJsFile; attrName = "zlib"; };
+          file = nijsImportPackage { inherit pkgsJsFile; attrName = "file"; };
+          perl = nijsImportPackage { inherit pkgsJsFile; attrName = "perl"; };
+          openssl = nijsImportPackage { inherit pkgsJsFile; attrName = "openssl"; };
+          curl = nijsImportPackage { inherit pkgsJsFile; attrName = "curl"; };
+          wget = nijsImportPackage { inherit pkgsJsFile; attrName = "wget"; };
+          sumTest = nijsImportPackage { inherit pkgsJsFile; attrName = "sumTest"; };
+          stringWriteTest = nijsImportPackage { inherit pkgsJsFile; attrName = "stringWriteTest"; };
+          appendFilesTest = nijsImportPackage { inherit pkgsJsFile; attrName = "appendFilesTest"; };
+          createFileWithMessageTest = nijsImportPackage { inherit pkgsJsFile; attrName = "createFileWithMessageTest"; };
+        };
+      };
+  };
+in
+jobs
