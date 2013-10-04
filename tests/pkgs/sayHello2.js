d30ba7c81bf4641c6c0f4cdc15fa3e90a29b1684
Sander van der Burg <svanderburg@gmail.com>
2013-10-04 10:45:24 +0200

    Add NixWith prototype + testcase

---
+++ tests/pkgs/sayHello2.js
@@ -0,0 +1,14 @@
+var nijs = require('../../lib/nijs.js');
+
+exports.pkg = function(args) {
+  return new nijs.NixWith({
+      withExpr: {
+        firstName: "Sander",
+        lastName: "van der Burg"
+      },
+      body: args.stdenv().mkDerivation({
+        name : "sayHello2",
+        buildCommand : "echo ${firstName} ${lastName} > $out"
+      })
+  });
+};
