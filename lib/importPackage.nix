99f0fe9c014709ebdbc6b489a070670e21cab518
Sander van der Burg <svanderburg@gmail.com>
2014-01-30 23:35:33 +0100

    Removed toString workarounds

--- lib/importPackage.nix
+++ lib/importPackage.nix
@@ -15,7 +15,7 @@ import (pkgs.stdenv.mkDerivation {
     name = "importPackage-${attrName}-buildCommand";
     requires = [
       { var = "fs"; module = "fs"; }
-      { var = "nijs"; module = "${toString ./.}/nijs.js"; }
+      { var = "nijs"; module = "${./.}/nijs.js"; }
       { var = "pkgsJsFile"; module = pkgsJsFile; }
     ];
     code = ''
@@ -24,8 +24,8 @@ import (pkgs.stdenv.mkDerivation {
       expr = new nijs.NixLet({
         value : {
           pkgs : new nijs.NixExpression('import ${nixpkgs} { system = "${system}"; }'),
-          nijsFunProxy : new nijs.NixExpression('import ${toString ./.}/funProxy.nix { inherit (pkgs) stdenv nodejs; }'),
-          nijsInlineProxy : new nijs.NixExpression('import ${toString ./.}/inlineProxy.nix { inherit (pkgs) stdenv writeTextFile nodejs; }')
+          nijsFunProxy : new nijs.NixExpression('import ${./.}/funProxy.nix { inherit (pkgs) stdenv nodejs; }'),
+          nijsInlineProxy : new nijs.NixExpression('import ${./.}/inlineProxy.nix { inherit (pkgs) stdenv writeTextFile nodejs; }')
         },
         body : expr
       });
