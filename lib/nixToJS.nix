35661e5ddb913571cf0a968487a1a515b5cca421
Sander van der Burg <svanderburg@gmail.com>
2014-02-04 14:47:26 +0100

    Use new builtins.typeOf operator

--- lib/nixToJS.nix
+++ lib/nixToJS.nix
@@ -3,15 +3,17 @@ expr:
 
 let
   nixToJS = expr:
-    if expr == null then "null"
-  else
-    if builtins.isBool expr then
+    let
+      exprType = builtins.typeOf expr;
+    in
+    if exprType == "int" then builtins.toString expr
+    else if exprType == "bool" then
       if expr then "true" else "false"
-    else if builtins.isInt expr then builtins.toString expr
-    else if builtins.isString expr then "\"${expr}\""
-    else if builtins.isList expr then "[ ${stdenv.lib.concatMapStrings (elem: nixToJS elem + ",\n") expr} ]"
-    else if builtins.isAttrs expr then "{ ${stdenv.lib.concatMapStrings (elem: "\"${elem}\" : ${nixToJS (builtins.getAttr elem expr)},\n") (builtins.attrNames expr)} }"
-    else if builtins.isFunction expr then throw "Cannot convert a Nix function to a JavaScript function!"
-    else "\"${expr}\""; # We assume it's a file
+    else if exprType == "string" then "\"${expr}\""
+    else if exprType == "path" then "new nijs.NixFile({ value: ${builtins.toString expr} })"
+    else if exprType == "set" then "{ ${stdenv.lib.concatMapStrings (elem: "\"${elem}\" : ${nixToJS (builtins.getAttr elem expr)},\n") (builtins.attrNames expr)} }"
+    else if exprType == "list" then "[ ${stdenv.lib.concatMapStrings (elem: nixToJS elem + ",\n") expr} ]"
+    else if exprType == "lambda" then "Cannot convert lambda to JavaScript"
+    else throw "Cannot convert this expression to JavaScript";
 in
 nixToJS expr
