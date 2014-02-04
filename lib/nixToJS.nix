3dc452405586a52fbebfd411fbec0590bd499175
Sander van der Burg <svanderburg@gmail.com>
2014-02-04 19:28:44 +0100

    Added null type handling

--- lib/nixToJS.nix
+++ lib/nixToJS.nix
@@ -11,6 +11,7 @@ let
       if expr then "true" else "false"
     else if exprType == "string" then "\"${expr}\""
     else if exprType == "path" then "new nijs.NixFile({ value: \"${builtins.toString expr}\" })"
+    else if exprType == "null" then "null"
     else if exprType == "set" then "{ ${stdenv.lib.concatMapStrings (elem: "\"${elem}\" : ${nixToJS (builtins.getAttr elem expr)},\n") (builtins.attrNames expr)} }"
     else if exprType == "list" then "[ ${stdenv.lib.concatMapStrings (elem: nixToJS elem + ",\n") expr} ]"
     else if exprType == "lambda" then "Cannot convert lambda to JavaScript"
