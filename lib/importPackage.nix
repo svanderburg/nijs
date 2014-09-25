af1e262d9447ff8ef41f806c11cfd9efc62239e6
Sander van der Burg <svanderburg@gmail.com>
2014-09-25 11:53:51 +0200

    Use AST facilities to compose these expressions

--- lib/importPackage.nix
+++ lib/importPackage.nix
@@ -19,16 +19,34 @@ import (pkgs.stdenv.mkDerivation {
       { var = "pkgsJsFile"; module = pkgsJsFile; }
     ];
     code = ''
-      var expr = pkgsJsFile.pkgs['${attrName}']();
-      
-      expr = new nijs.NixLet({
+      var expr = new nijs.NixLet({
         value : {
-          pkgs : new nijs.NixExpression('import ${nixpkgs} { system = "${system}"; }'),
-          nijs : new nijs.NixExpression('builtins.storePath ${nijs}'),
-          nijsFunProxy : new nijs.NixExpression('import ${nijs}/lib/node_modules/nijs/lib/funProxy.nix { inherit (pkgs) stdenv nodejs; inherit nijs; }'),
-          nijsInlineProxy : new nijs.NixExpression('import ${nijs}/lib/node_modules/nijs/lib/inlineProxy.nix { inherit (pkgs) stdenv writeTextFile nodejs; inherit nijs; }')
+          pkgs : new nijs.NixFunInvocation({
+            funExpr: new nijs.NixImport('${nixpkgs}'),
+            paramExpr: {
+              system: '${system}'
+            }
+          }),
+          nijs : new nijs.NixStorePath('${nijs}'),
+          nijsFunProxy: nijs.NixFunInvocation({
+            funExpr: new nijs.NixImport("${nijs}/lib/node_modules/nijs/lib/funProxy.nix"),
+            paramExpr: {
+              stdenv: new nijs.NixInherit("pkgs"),
+              nodejs: new nijs.NixInherit("pkgs"),
+              nijs: new nijs.NixInherit()
+            }
+          }),
+          newInlineProxy : new nijs.NixFunInvocation({
+            funExpr: new nijs.NixImport("${nijs}/lib/node_modules/nijs/lib/inlineProxy.nix"),
+            paramExpr: {
+              stdenv: new nijs.NixInherit("pkgs"),
+              writeTextFile: new nijs.NixInherit("pkgs"),
+              nodejs: new nijs.NixInherit("pkgs"),
+              nijs: new nijs.NixInherit()
+            }
+          })
         },
-        body : expr
+        body : pkgsJsFile.pkgs['${attrName}']()
       });
 
       fs.writeFileSync(process.env['out'], nijs.jsToNix(expr));
--- lib/importPackageAsync.nix
+++ lib/importPackageAsync.nix
@@ -29,10 +29,30 @@ import (pkgs.stdenv.mkDerivation {
           function(callback, _expr) {
               var expr = new nijs.NixLet({
                   value : {
-                      pkgs : new nijs.NixExpression('import ${nixpkgs} { system = "${system}"; }'),
-                      nijs : new nijs.NixExpression('builtins.storePath ${nijs}'),
-                      nijsFunProxy : new nijs.NixExpression('import ${nijs}/lib/node_modules/nijs/lib/funProxy.nix { inherit (pkgs) stdenv nodejs; inherit nijs; }'),
-                      nijsInlineProxy : new nijs.NixExpression('import ${nijs}/lib/node_modules/nijs/lib/inlineProxy.nix { inherit (pkgs) stdenv writeTextFile nodejs; inherit nijs; }')
+                      pkgs : new nijs.NixFunInvocation({
+                          funExpr: new nijs.NixImport('${nixpkgs}'),
+                          paramExpr: {
+                              system: '${system}'
+                          }
+                      }),
+                      nijs : new nijs.NixStorePath('${nijs}'),
+                      nijsFunProxy: nijs.NixFunInvocation({
+                          funExpr: new nijs.NixImport("${nijs}/lib/node_modules/nijs/lib/funProxy.nix"),
+                          paramExpr: {
+                              stdenv: new nijs.NixInherit("pkgs"),
+                              nodejs: new nijs.NixInherit("pkgs"),
+                              nijs: new nijs.NixInherit()
+                          }
+                      }),
+                      newInlineProxy : new nijs.NixFunInvocation({
+                          funExpr: new nijs.NixImport("${nijs}/lib/node_modules/nijs/lib/inlineProxy.nix"),
+                          paramExpr: {
+                              stdenv: new nijs.NixInherit("pkgs"),
+                              writeTextFile: new nijs.NixInherit("pkgs"),
+                              nodejs: new nijs.NixInherit("pkgs"),
+                              nijs: new nijs.NixInherit()
+                          }
+                      })
                   },
                   body : _expr
               });
